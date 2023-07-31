f627003dd2f21980c2f5c0614fa8de89c5a1090a
matthew bottaro <johnlarksnow@gmail.com>
2023-07-31 12:57:28 -0800

    test5

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
+<template>
+  <main class="bg-green-100 dark:text-white text-gray-900 font-inter min-h-screen dark:bg-[#050505]">
+    <NuxtLoadingIndicator v-if="loading" />
+    <NuxtLayout class="h-full">
+      <NuxtPage />
+    </NuxtLayout>
+  </main>
+</template>
+
 <script setup lang="ts">
-import { watch } from 'vue'
+import { defineComponent, watch, ref } from 'vue'
 const nuxtApp = useNuxtApp()
 const loading = ref(false)
 
@@ -12,35 +21,51 @@ addRouteMiddleware('global-loader', () => {
 nuxtApp.hook('page:finish', () => {
   loading.value = false
 })
-
-// watching the loading ref, and using the value to control nuxtApp's loading status
-watch(loading, (newVal) => {
-  nuxtApp.$nuxt.$loading.start()
-  if (!newVal) {
-    nuxtApp.$nuxt.$loading.finish()
-  }
-})
 </script>
 
-<template>
-  <main class="bg-green-100 dark:text-white text-gray-900 font-inter min-h-screen dark:bg-[#050505]">
-    <NuxtLoadingIndicator v-if="loading" />
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-    <NuxtLayout class="h-full">
-      <NuxtPage />
-    </NuxtLayout>
-  </main>
-</template>
+export default defineComponent({
+  name: 'App',
+  head() {
+    return {
+      title: 'Your App Title', // Set your desired title here
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+        { hid: 'description', name: 'description', content: 'Your app description' },
+        { hid: 'og:title', property: 'og:title', content: 'Open Graph Title' }, // Open Graph Title (for social media)
+        { hid: 'og:description', property: 'og:description', content: 'Open Graph Description' }, // Open Graph Description (for social media)
+        { hid: 'og:image', property: 'og:image', content: 'https://your-app-url.com/icon.png' }, // Open Graph Image (for social media)
+        { name: 'twitter:card', content: 'summary_large_image' }, // Twitter card type (for social media)
+        { name: 'twitter:title', content: 'Twitter Title' }, // Twitter Title (for social media)
+        { name: 'twitter:description', content: 'Twitter Description' }, // Twitter Description (for social media)
+        { name: 'twitter:image', content: 'https://your-app-url.com/icon.png' }, // Twitter Image (for social media)
+        // Add any other meta tags you need
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: 'https://your-app-url.com/icon.png' }, // Set your app icon URL here
+        // Add any other link tags you need
+      ],
+    }
+  },
+});
+</script>
+
+
+
 
 <style>
 html {
-    scroll-behavior: smooth;
+  scroll-behavior: smooth;
 }
 
 .page-enter-active,
 .page-leave-active {
   transition: opacity 0.5s ease;
 }
+
 .page-enter-from,
 .page-leave-to {
   opacity: 0;
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default{
+  head: {
+    title: 'Spotigrade', 
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+      { hid: 'description', name: 'description', content: 'Your app description' },
+      { hid: 'og:title', property: 'og:title', content: 'Spotigrade | Premium Spotify Accounts' }, // Open Graph Title (for social media)
+      { hid: 'og:description', property: 'og:description', content: 'SpotiGrade provides high-quality Spotify upgrades directly to your personal account, adding Lifetime Warranty with Auto-Renewal. Get to enjoy our service with the easy-to-use user interface and our cheap pricing.      ' }, // Open Graph Description (for social media)
+      { hid: 'og:image', property: 'og:image', content: 'https://i.imgur.com/uG1AGSR.png' }, // Open Graph Image (for social media)
+      { name: 'twitter:card', content: 'summary_large_image' }, // Twitter card type (for social media)
+      { name: 'twitter:title', content: 'Twitter Title' }, // Twitter Title (for social media)
+      { name: 'twitter:description', content: 'Twitter Description' }, // Twitter Description (for social media)
+      { name: 'twitter:image', content: 'https://i.imgur.com/uG1AGSR.png' }, // Twitter Image (for social media)
+    ],
+    link: [
+      { rel: 'icon', type: 'image/png', href: 'https://i.imgur.com/uG1AGSR.png' }, // Set your app icon URL here
+    ],
+  },
   modules: [
     '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
@@ -38,4 +55,4 @@ export default defineNuxtConfig({
       mode: 'out-in',
     },
   },
-})
+};
