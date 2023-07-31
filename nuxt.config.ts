fc5050d0f33a4f949ed64aa2a2fb3b45415d1b1d
matthew bottaro <johnlarksnow@gmail.com>
2023-07-30 16:04:07 -0800

    sss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
-  nitro: {
-    preset: 'service-worker'
-  },
   modules: [
     '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
