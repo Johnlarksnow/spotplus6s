d0b2c9884a61c28cf097685be8399dbea4bd36d7
matthew bottaro <johnlarksnow@gmail.com>
2023-08-01 10:28:56 -0800

    test13

--- composables/buy.ts
+++ composables/buy.ts
@@ -1,13 +1,12 @@
 export async function useBuy(plan: string, amount: string) {
   const authStore = useAuthStore()
 
-  const { data } = await useFetch('https://supgrade-api.site/api/v1/payments/create', {
+  const { data } = await useFetch('https://spotigrade-api.xyz/api/v1/payments/create', {
     method: 'POST',
     body: JSON.stringify({
       plan,
       amount,
-      userId: authStore.decodeJwt().id,
-    }),
+      userId: authStore.decodeJwt().id,    }),
     headers: {
       Authorization: `Bearer ${authStore.decodeJwt().id}`,
     },
